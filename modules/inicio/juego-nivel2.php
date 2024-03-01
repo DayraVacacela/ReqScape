4d7726900b5faf52d6e06b480b080de52c8d488d
bethsaida03 <bethsaida.cheam@ug.edu.ec>
2024-03-01 14:24:51 -0500

    Front de la pantalla del juego nivel 2

---
+++ modules/inicio/juego-nivel2.php
@@ -0,0 +1,47 @@
+<head>
+<link rel="stylesheet" type="text/css" href="../../styles/juego-nivel2.css">
+</head>
+
+<div class="fila1">
+    <div class="cash">
+    210
+    </div>
+    <div class="coin">
+        <img src="../../assets/img/juego-lvl1/coin.png" alt="">
+    </div>
+</div>
+<div class="fila2">
+    <span class="titulo-nivel-txt">
+        Nivel 02
+    </span>
+</div>
+<div class="box-ops">
+    <div class="box-ops-f1">
+        <div class="box-ops-f1-cl1">
+            Req..........................................
+        </div>
+        <div class="box-ops-f1-cl2">
+            opción respuesta................
+        </div>
+        <div class="box-ops-f1-cl1">
+            Req..........................................
+        </div>
+        <div class="box-ops-f1-cl2">
+            opción respuesta................
+        </div>
+    </div>
+    <div class="box-ops-f2">
+        <div class="box-ops-f1-cl1">
+            Req..........................................
+        </div>
+        <div class="box-ops-f1-cl2">
+            opción respuesta................
+        </div>
+        <div class="box-ops-f1-cl1">
+            Req..........................................
+        </div>
+        <div class="box-ops-f1-cl2">
+            opción respuesta................
+        </div>
+    </div>
+</div>
