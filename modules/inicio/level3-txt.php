8424a87d2b0027526aa38a9e6da18fc12a8ba827
bethsaida03 <bethsaida.cheam@ug.edu.ec>
2024-03-01 14:28:26 -0500

    front de la pantalla de texto del nivel 3

---
+++ modules/inicio/level3-txt.php
@@ -0,0 +1,24 @@
+<head>
+<link rel="stylesheet" type="text/css" href="../../styles/level3-txt.css">
+</head>
+<div class="fila1">
+    <span class="titulo-nivel-txt">
+        Nivel 03
+    </span>
+</div>
+<div class="fila2">
+    <div class="fila2-1">
+        <span class="texto1-txt">
+            TU TERCERA TAREA ES ORDENAR EL REQUERIMIENTO NO AMBIGUO
+        </span>
+    </div>
+    <div class="fila2-2">
+        <form method="post">
+            <button type="submit" class="btn-icono" name="cambiarTexto">
+                SALTAR
+                <img src="../../assets/img/lever1-txt/saltar_btn.png" alt="Icono" />
+            </button>
+          
+        </form>
+    </div>
+</div>
