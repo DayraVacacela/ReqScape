9387df995c3f92045ff07a6fc8216d8dfd035e01
bethsaida03 <bethsaida.cheam@ug.edu.ec>
2024-03-01 13:31:51 -0500

    Creación del contenido (front) del juego nivel 1.

---
+++ modules/inicio/juego-nivel1.php
@@ -0,0 +1,51 @@
+<head>
+<link rel="stylesheet" type="text/css" href="../../styles/juego-nivel1.css">
+</head>
+
+<div class="fila1">
+    <div class="cash">
+    210
+    </div>
+    <div class="coin">
+        <img src="../../assets/img/juego-lvl1/coin.png" alt="">
+    </div>
+</div>
+<div class="fila2">
+    <span class="titulo-nivel-txt">
+        Nivel 01
+    </span>
+</div>
+<div class="fila3">
+    <div class="fila3-cl1">
+        <div class="fila3-cl1-f1">
+            El sistema debe permitir a los usuarios realizar búsquedas eficientes de libros.
+        </div>
+        <div class="fila3-cl1-f2">
+            El sistema debe permitir a los usuarios realizar búsquedas eficientes de libros.
+        </div>
+        <div class="fila3-cl1-f3">
+            El sistema debe permitir a los usuarios realizar búsquedas eficientes de libros.
+        </div>
+        <div class="fila3-cl1-f4">
+            El sistema debe permitir a los usuarios realizar búsquedas eficientes de libros.
+        </div>
+    </div>
+    <div class="fila3-cl2">
+        <div class="fila3-cl1-cl1">
+            <div class="img-cesto">
+                <img src="../../assets/img/juego-lvl1/cesto.png" alt="" class="img-cesto">
+            </div>   
+            <span class="texto-img-cesto">
+                AMBIGUO
+            </span>
+        </div>
+        <div class="fila3-cl1-cl2">
+            <div class="img-cesto">
+                <img src="../../assets/img/juego-lvl1/cesto.png" alt="">
+            </div>
+            <span class="texto-img-cesto">
+                NO AMBIGUO
+            </span>
+        </div>
+    </div>
+</div>
