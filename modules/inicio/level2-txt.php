56657bb447460351cc02eebad26ed1d980bc0536
bethsaida03 <bethsaida.cheam@ug.edu.ec>
2024-03-01 13:38:00 -0500

    Creación de pantalla del nivel dos txt.

---
+++ modules/inicio/level2-txt.php
@@ -0,0 +1,24 @@
+<head>
+<link rel="stylesheet" type="text/css" href="../../styles/level2-txt.css">
+</head>
+<div class="fila1">
+    <span class="titulo-nivel-txt">
+        Nivel 02
+    </span>
+</div>
+<div class="fila2">
+    <div class="fila2-1">
+        <span class="texto1-txt">
+            TU SEGUNDA TAREA ES EMPAREJAR LOS REQUERIMIENTOS AMBIGUOS CON LOS REQUERIMIENTOS CORRECTOS
+        </span>
+    </div>
+    <div class="fila2-2">
+        <form method="post">
+            <button type="submit" class="btn-icono" name="cambiarTexto">
+                SALTAR
+                <img src="../../assets/img/lever1-txt/saltar_btn.png" alt="Icono" />
+            </button>
+          
+        </form>
+    </div>
+</div>
