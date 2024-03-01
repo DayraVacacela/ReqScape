aa21c4bf08deb449cf95077f69a9f5396af00653
bethsaida03 <bethsaida.cheam@ug.edu.ec>
2024-03-01 14:35:33 -0500

    front de la pantalla juego nivel 3

---
+++ modules/inicio/juego-nivel3.php
@@ -0,0 +1,47 @@
+<head>
+<link rel="stylesheet" type="text/css" href="../../styles/juego-nivel3.css">
+</head>
+
+<div class="fila1">
+    <div class="cash">
+    210
+    </div>
+    <div class="coin">
+        <img src="../../assets/img/juego-lvl1/coin.png" alt="">
+    </div>
+</div>
+<div class="fila2">
+    <span class="titulo-nivel-txt">
+        Nivel 03
+    </span>
+</div>
+<div class="box-ops">
+    <div class="box-ops-f1">
+        <div class="box-ops-f1-cl1">
+            Req..........................................
+        </div>
+        <div class="box-ops-f1-cl2">
+            opción respuesta................
+        </div>
+        <div class="box-ops-f1-cl3">
+            Req..........................................
+        </div>
+        <div class="box-ops-f1-cl4">
+            opción respuesta................
+        </div>
+    </div>
+    <div class="box-ops-f2">
+        <div class="box-ops-f2-cl1">
+            
+        </div>
+        <div class="box-ops-f2-cl2">
+            
+        </div>
+        <div class="box-ops-f2-cl3">
+            
+        </div>
+        <div class="box-ops-f2-cl4">
+            
+        </div>
+    </div>
+</div>
