cfe0a189889abd3438fb4f47673ffa7db1b73a2d
bethsaida03 <bethsaida.cheam@ug.edu.ec>
2024-03-03 11:55:09 -0500

    Juego nivel 5 y 6 (front)

---
+++ modules/inicio/level5-txt.php
@@ -0,0 +1,24 @@
+<head>
+<link rel="stylesheet" type="text/css" href="../../styles/level-txt.css">
+</head>
+<div class="fila1">
+    <span class="titulo-nivel-txt">
+        Nivel 05
+    </span>
+</div>
+<div class="fila2">
+    <div class="fila2-1">
+        <span class="texto1-txt">
+            TU QUINTA TAREA ES SELECCIONAR "LOS DETALLES DE REQUISITOS Y RESTRICCIONES" ACORDES AL REQUERIMIENTO BRINDADO.
+        </span>
+    </div>
+    <div class="fila2-2">
+        <form method="post">
+            <button type="submit" class="btn-icono" name="cambiarTexto">
+                SALTAR
+                <img src="../../assets/img/lever1-txt/saltar_btn.png" alt="Icono" />
+            </button>
+          
+        </form>
+    </div>
+</div>
