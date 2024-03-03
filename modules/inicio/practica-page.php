707789ce6583429a29bb288ac8c10dcd7bea8362
bethsaida03 <bethsaida.cheam@ug.edu.ec>
2024-03-03 12:26:56 -0500

    pantalla de práctica (Front)

---
+++ modules/inicio/practica-page.php
@@ -0,0 +1,28 @@
+<head>
+<link rel="stylesheet" type="text/css" href="../../styles/practica-page.css">
+</head>
+
+<div class="fila1">
+    <div class="cash">
+    210
+    </div>
+    <div class="coin">
+        <img src="../../assets/img/juego-lvl1/coin.png" alt="">
+    </div>
+</div>
+<div class="fila2">
+    <span class="titulo-nivel-txt">
+        PRÁCTICA
+    </span>
+</div>
+<div class="contenedor">
+    <div class="texto-req">
+        <span class="txt">EL SISTEMA DEBE PERMITIR A LOS USUARIOS REALIZAR BÚSQUEDAS EFICIENTES DE LIBROS.</span>
+    </div>
+    <div class="btn-1">
+        <button>AMBIGUO</button>
+    </div>
+    <div class="btn-2">
+        <button>NO AMBIGUO</button>
+    </div>
+</div>
